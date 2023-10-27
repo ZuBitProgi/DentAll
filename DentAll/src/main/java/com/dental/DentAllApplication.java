4a3f2703698b8ba23b21f8ef25121374530c3371
Viulica <dermit.stela@gmail.com>
2023-10-27 13:47:20 +0200

    Složen spring boot projekt

---
+++ DentAll/src/main/java/com/dental/DentAllApplication.java
@@ -0,0 +1,13 @@
+package com.dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentAllApplication.class, args);
+	}
+
+}
---
+++ DentAll/src/test/java/com/dental/DentAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
