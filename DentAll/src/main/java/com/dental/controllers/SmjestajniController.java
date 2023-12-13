232b1c7380e16a455bf470a9166e2517204b2d91
blueFlower11 <133530289+blueFlower11@users.noreply.github.com>
2023-12-13 02:04:37 +0100

    dodan controler za smjestaj

---
+++ DentAll/src/main/java/com/dental/controllers/SmjestajniController.java
@@ -0,0 +1,37 @@
+package com.dental.controllers;
+
+import java.util.List;
+
+import com.dental.models.Smjestaj;
+import com.dental.service.SmjestajService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accomodation")
+public class SmjestajniController {
+
+    @Autowired
+    private SmjestajService smjestajservis;
+
+    @GetMapping("")
+    public List<Smjestaj> listSmjestaj(){
+        return smjestajservis.listAll();
+    }
+
+    @GetMapping("/Id")
+    public List<Smjestaj> getSmjestajById(@RequestBody Integer id){
+        return smjestajservis.findSmjestajById(id);
+    }
+
+    @PostMapping("/create")
+    public Smjestaj createSmjestaj(@RequestBody Smjestaj smjestaj){
+        return smjestajservis.createSmjestaj(smjestaj);
+    }
+
+    @PostMapping("/delete")
+    public void deleteSmjestaj(@RequestBody Integer id){
+        smjestajservis.deleteSmjestaj(id);
+    }
+}
+
