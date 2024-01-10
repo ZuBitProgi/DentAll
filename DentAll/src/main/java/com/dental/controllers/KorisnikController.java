dcb5868d8cf7dbb6bc3f5adf6a61c8d66aef4c0f
vFer11 <147745878+vFer11@users.noreply.github.com>
2024-01-11 00:48:05 +0100

    logika dodavanja putovanja

--- DentAll/src/main/java/com/dental/controllers/KorisnikController.java
+++ DentAll/src/main/java/com/dental/controllers/KorisnikController.java
@@ -1,11 +1,22 @@
 package com.dental.controllers;
 
+import com.dental.dao.KlinikaDao;
+import com.dental.dao.PrijevoznikDao;
+import com.dental.dao.PrijevoznikDaoImpl;
+import com.dental.dao.SmjestajDaoImpl;
+import com.dental.models.Klinika;
 import com.dental.models.Korisnik;
+import com.dental.models.Prijevoznik;
+import com.dental.models.Putovanje;
 import com.dental.service.KorisnikService;
 import com.dental.service.PutovanjeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -14,8 +25,6 @@ public class KorisnikController {
 
     @Autowired
     private KorisnikService korisnikService;
-
-
 
     @GetMapping("")
     public List<Korisnik> listKorisnik(){
@@ -29,8 +38,23 @@ public class KorisnikController {
 
     @PostMapping("/create")
     public Korisnik createKorisnik(@RequestBody Korisnik korisnik){
-
-        return korisnikService.createKorisnik(korisnik);
+        korisnikService.createKorisnik(korisnik);
+        String kategorija = null, tip = null, kapacitet = null, adresa = null;
+        Boolean dostupnost = true;
+        LocalTime vrijeme = null;
+        for(String p : korisnik.getPreference().split(",")){
+            if(p.split(":")[0].equals("kategorija")) kategorija = p.split(":")[1];
+            else if(p.split(":")[0].equals("tip")) tip = p.split(":")[1];
+            else if(p.split(":")[0].equals("kapacitet")) kapacitet = p.split(":")[1];
+            else if(p.split(":")[0].equals("adresa")) adresa= p.split(":")[1];
+            else vrijeme = LocalTime.parse(p.split(":")[1]);
+        }
+        PutovanjeService putovanje = new PutovanjeService();
+        KlinikaDao klinika = new KlinikaDao();
+        PrijevoznikDaoImpl prijevoznik = new PrijevoznikDaoImpl();
+        SmjestajDaoImpl smjestaj = new SmjestajDaoImpl();
+        putovanje.add(new Putovanje(Time.valueOf(vrijeme), klinika.findKlinikaByAdresa(adresa).getId(), prijevoznik.findPrijevoznikByVozilo(kapacitet).getId(), smjestaj.findSmjestajByKategorijaTipDostupnost(kategorija, tip, dostupnost).getId(), korisnik.getId(), "tamo"));
+        return korisnikService.findKorisnikById(korisnik.getId());
     }
 
     @PostMapping("/delete")
