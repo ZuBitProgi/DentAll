1ed2becb1e798b7dc750d64da4ae76ee7f84b1a9
vFer11 <147745878+vFer11@users.noreply.github.com>
2024-01-11 00:49:50 +0100

    za logiku

--- DentAll/src/main/java/com/dental/dao/PrijevoznikDao.java
+++ DentAll/src/main/java/com/dental/dao/PrijevoznikDao.java
@@ -8,5 +8,7 @@ public interface PrijevoznikDao {
      void deletePrijevoznik(Integer id);
      List<Prijevoznik> findAll();
      Prijevoznik findPrijevoznikById(Integer id);
+
+     Prijevoznik findPrijevoznikByVozilo(String kapacitet);
      Prijevoznik create(Prijevoznik prijevoznik);
 }
